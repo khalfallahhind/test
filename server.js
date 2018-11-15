0910e3a28cdd6036eeea25a1354c6ba450ccb188
bnd <khalfallahhind12@gmail.com>
2018-11-15 14:21:29 +0100

    add project page

--- server.js
+++ server.js
@@ -1,71 +1,51 @@
 const express = require('express');
-const hbs =require('hbs');
-const fs=require('fs');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
 var app = express();
-
 hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n',(err)=>
-  {
-if(err){
-  console.log('unableto appendto server.log.')
-}
-
-  });
-
+  fs.appendFile('server.log', log + '\n');
   next();
 });
- //app.use((req, res, next) => {
-  // res.render('maintenance.hbs');
-//});
 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
+app.use(express.static(__dirname + '/public'));
 
-
-
-//registerhelper pour declarer des fonction à utiliser
-hbs.registerHelper('getCurrentYear', () =>{
-  return new Date().getFullYear()
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
-
-hbs.registerHelper('screamIt', (text) =>{
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-
-
 app.get('/', (req, res) => {
-  res.render('home.hbs',{
-    pageTitle: 'home Page',
-    welcomeMessage:('hii'),
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
-
-
-
-  // res.send('<h1>Hello Express!</h1>');
-  /*res.send({
-    name: 'Andrew',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
-  });
-});*/
-
 app.get('/about', (req, res) => {
-  res.render('about.hbs',{
-    pageTitle: 'About Page',
-    welcomeMessage:('hii')
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'projects Page'
   });
 });
 
@@ -76,5 +56,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');});
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
+});
